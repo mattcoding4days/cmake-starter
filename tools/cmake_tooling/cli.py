93846d072acaf0da19e0834c5918063529e37b2d
Matt Williams <mattltf@protonmail.com>
2022-03-21 15:40:48 -0700

    :neckbeard: Creating sub-arguments

--- tools/cmake_tooling/cli.py
+++ tools/cmake_tooling/cli.py
@@ -18,17 +18,20 @@ class Cli:
             description="Helper command line tool to be used by cortex developers and in the ci/cd pipeline",
             allow_abbrev=False,
         )
+
+        # create subcommand parser
+        sub_commands = parser.add_subparsers(help="Available subcommands")
+
+        # create the clang tooling commands
+        clang_commands = sub_commands.add_parser("clang", help="Execute linting and static analyzing with clang tooling")
+        clang_commands.add_argument("--format", action="store_true", help="Format all projects recursively")
+        clang_commands.add_argument("--lint", action="store_true", help="Lints all files")
+
         parser.add_argument(
-            "-l",
+            "-",
             "--list-sessions",
             help="show all vim session files in VIM_SESSIONS directory",
             action="store_true",
-        )
-        parser.add_argument(
-            "-r", "--remove-session", help="remove a vim session file by name", type=str
-        )
-        parser.add_argument(
-            "-o", "--open-session", help="open a vim session file by name", type=str
         )
         self.__args = parser.parse_args()
 
